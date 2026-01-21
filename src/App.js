4685577efaf3c162a974883ad2e3bd3d4aded4cb
Manish Kumar <manish.rajput62647457@gmail.com>
2026-01-21 21:55:22 +0530

    Fix router for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Home from './screens/Home';
 import Login from './screens/Login';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
@@ -11,15 +11,14 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import Signup from './screens/Signup';
 
-
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route exact path='/' element={<Home/>}/>
-          <Route exact path='/login' element={<Login/>}/>
-          <Route exact path='/createuser' element={<Signup/>}/>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/createuser' element={<Signup />} />
         </Routes>
       </div>
     </Router>
